2fead0e4701a6e6644fecae96630648cdd09c50c
Patrick Fenner <patrick@defproc.co.uk>
2015-10-05 11:29:52 +0100

    removed swap files and added them to the gitignore

--- arduino/button/.build_ano/uno/arduino/wiring.d
+++
@@ -1,5 +0,0 @@
-.build_ano/uno/arduino/wiring.d .build_ano/uno/arduino/wiring.o: /usr/share/arduino/hardware/arduino/avr/cores/arduino/wiring.c \
- /usr/share/arduino/hardware/arduino/avr/cores/arduino/wiring_private.h \
- /usr/share/arduino/hardware/arduino/avr/cores/arduino/Arduino.h \
- /usr/share/arduino/hardware/arduino/avr/cores/arduino/binary.h \
- /usr/share/arduino/hardware/arduino/avr/variants/standard/pins_arduino.h
--- arduino/receiver/.build_ano/uno/arduino/abi.d
+++
@@ -1,1 +0,0 @@
-.build_ano/uno/arduino/abi.d .build_ano/uno/arduino/abi.o: /usr/share/arduino/hardware/arduino/avr/cores/arduino/abi.cpp
